071fa3e3f8d849a550d1449ca2f611358d50e13f
Thameera Senanayaka <thameera123@gmail.com>
2017-02-05 20:29:33 +0530

    Style Status column in colors

--- client/src/components/Grid/Grid.js
+++ client/src/components/Grid/Grid.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import ReactDataGrid from 'react-data-grid';
 import './Grid.css';
 
+const StatusFormatter = ({value}) => {
+  return (
+    <div className={`status status-${value}`}>
+      {value}
+    </div>
+  );
+}
+
 const columns = [
   { key: 'id', name: 'ID', width: 70, locked: true },
-  { key: 'status', name: 'Status', width: 120 },
+  { key: 'status', name: 'Status', width: 100, formatter: StatusFormatter },
   { key: 'priority', name: 'Priority', width: 80 },
   { key: 'subject', name: 'Subject', editable: true, width: 250 },
   { key: 'tags', name: 'Tags', editable: true, width: 200 },
