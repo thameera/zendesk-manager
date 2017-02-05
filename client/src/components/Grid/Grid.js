2058a18de5139e9096a03234fa4a71ca29e7139e
Thameera Senanayaka <thameera123@gmail.com>
2017-02-05 20:23:27 +0530

    Grid styling

--- client/src/components/Grid/Grid.js
+++ client/src/components/Grid/Grid.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 import ReactDataGrid from 'react-data-grid';
+import './Grid.css';
 
 const columns = [
-  { key: 'id', name: 'ID', width: 80 },
+  { key: 'id', name: 'ID', width: 70, locked: true },
   { key: 'status', name: 'Status', width: 120 },
-  { key: 'subject', name: 'Subject' },
-  { key: 'priority', name: 'Priority' },
-  { key: 'tags', name: 'Tags', editable: true },
-  { key: 'next', name: 'Next', editable: true },
+  { key: 'priority', name: 'Priority', width: 80 },
+  { key: 'subject', name: 'Subject', editable: true, width: 250 },
+  { key: 'tags', name: 'Tags', editable: true, width: 200 },
+  { key: 'next', name: 'Next', editable: true, width: 800 },
 ]
 
 class Grid extends Component {
   render() {
     return (
-      <div>
+      <div className="grid">
         <ReactDataGrid
           enableCellSelect={true}
           columns={columns}
           rowGetter={this.props.ticketGetter}
           rowsCount={this.props.ticketCount}
           onRowUpdated={this.props.handleRowUpdate}
+          minHeight={550}
         />
       </div>
     );
